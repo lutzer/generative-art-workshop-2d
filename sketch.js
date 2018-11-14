fa6d4165fcb4490640360296c7b408e22288c327
Lutz Reiter <lutz.reiter@vw-fc.com>
2018-11-14 10:57:37 +0100

    Add cool palettes

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 const canvasSketch = require('canvas-sketch');
-const  { lerp } = require('canvas-sketch-util/math')
+const  { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
+
+random.setSeed(random.getRandomSeed());
 
 const settings = {
+  suffix: random.getSeed(),
   dimensions: [ 512, 512 ]
 };
 
 const sketch = () => {
+  const palette = random.pick(palettes);
+  const background = palette.shift();
+
+  console.log(palette);
 
   const createGrid = (count = 30) => {
     const points = [];
@@ -13,30 +22,33 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count -1);
-        points.push([u, v]);
+        points.push({
+          color: random.pick(palette),
+          radius: Math.max(0, 10 + random.gaussian() * 5),
+          position: [u, v]
+        });
       }
     }
     return points;
   }
 
-  const grid = createGrid().filter( () => Math.random() > 0.5);
+  const grid = createGrid().filter( () => random.chance(0.3));
 
   return ({ context, width, height }) => {
     const minDim = Math.min(width,height);
-    context.fillStyle = 'black';
+    context.fillStyle = background;
     context.fillRect(0, 0, width, height);
 
-    const margin = 0.1 * width;
-    grid.forEach(point => {
-      [u, v] = point;
+    const margin = 0.2 * width;
+    grid.forEach( ({ position, color, radius }) => {
+      [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2)
-      context.lineWidth = 2;
-      context.strokeStyle = 'white';
-      context.stroke();
+      context.arc(x, y, radius, 0, Math.PI * 2)
+      context.fillStyle = color;
+      context.fill();
     });
   };
 };
