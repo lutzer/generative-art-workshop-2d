8e9851120e3d5485adcd7381e6439abf16fe66fc
Lutz Reiter <lutz.reiter@vw-fc.com>
2018-11-14 10:24:50 +0100

    Created grid

--- sketch.js
+++ sketch.js
@@ -1,26 +1,42 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  // dimensions: [ 2048, 2048 ]
-  dimensions: "postcard",
-  orientation: "landscape",
-  pixelsPerInch: 300,
-  units: 'cm'
+  dimensions: [ 512, 512 ]
 };
 
 const sketch = () => {
+
+  const createGrid = (count = 5) => {
+    const points = [];
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count -1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  }
+
+  const grid = createGrid();
+  console.log(grid);
+
+
   return ({ context, width, height }) => {
     const minDim = Math.min(width,height);
-    context.fillStyle = 'black';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, minDim * 0.2, 0, 2 * Math.PI);
-    context.fillStyle = "white";
-    context.fill();
-    context.strokeStyle = "gray";
-    context.lineWidth = 0.2;
-    context.stroke();
+    grid.forEach(point => {
+      [u, v] = point;
+      [x, y] = [u * width, v * height];
+
+      context.beginPath();
+      context.arc(x, y, 10, 0, Math.PI * 2)
+      context.lineWidth = 5;
+      context.strokeStyle = 'black';
+      context.stroke();
+    });
   };
 };
 
